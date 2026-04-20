43ffebf22048761e6f0da2390feb261475662c67
WillaimPGallardo <willijuan2011@hotmail.com>
2026-04-20 09:02:38 -0500

    COMMIT 4
    git add .
    git commit -m "Controlador MVC con rutas CRUD y PRG implementado"

--- productos-web/src/main/java/com/universidad/productos_web/controller/ProductoController.java
+++ productos-web/src/main/java/com/universidad/productos_web/controller/ProductoController.java
@@ -1,5 +1,51 @@
-package com.universidad.productos_web.controller;
+package com.universidad.productosweb.controller;
 
+import com.universidad.productosweb.model.Producto;
+import com.universidad.productosweb.service.ProductoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+@RequestMapping("/productos")
 public class ProductoController {
-    
+
+    @Autowired
+    private ProductoService servicio;
+
+    @GetMapping
+    public String listar(Model model) {
+        model.addAttribute("productos", servicio.obtenerTodos());
+        return "productos/lista";
+    }
+
+    @GetMapping("/nuevo")
+    public String nuevo(Model model) {
+        model.addAttribute("producto", new Producto());
+        model.addAttribute("accion", "Crear");
+        return "productos/formulario";
+    }
+
+    @GetMapping("/editar/{id}")
+    public String editar(@PathVariable Long id, Model model) {
+        Producto producto = servicio.buscarPorId(id)
+                .orElseThrow(() -> new RuntimeException("No encontrado"));
+
+        model.addAttribute("producto", producto);
+        model.addAttribute("accion", "Editar");
+        return "productos/formulario";
+    }
+
+    @PostMapping("/guardar")
+    public String guardar(@ModelAttribute Producto producto) {
+        servicio.guardar(producto);
+        return "redirect:/productos";
+    }
+
+    @GetMapping("/eliminar/{id}")
+    public String eliminar(@PathVariable Long id) {
+        servicio.eliminar(id);
+        return "redirect:/productos";
+    }
 }
